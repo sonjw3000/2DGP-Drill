e68020e7ddceeda471daefa7a0d564d465d48bee
Jungwon Desktop <sonjw3000@gmail.com>
2021-09-16 16:01:21 +0900

    lec04

--- 06/character_moves.py
+++ 06/character_moves.py
@@ -1,10 +1,17 @@
 from pico2d import *
 
 open_canvas()
-
+hide_lattice()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
 # fill here
-    
+x = 0
+while (x < 800):
+    clear_canvas_now()              # Game Rendering
+    grass.draw_now(400, 30)
+    character.draw_now(x, 90)
+    x = x + 2                       # Game Logic
+    delay(0.01)
+
 close_canvas()
